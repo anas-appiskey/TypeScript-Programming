992bf361721e3023d0f10e18b54c4614ab65f726
anasali2193 <anasali2193@gmail.com>
2020-12-03 22:08:38 +0500

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # TypeScript-Programming
  
+
+
+## Project Learnings
+
+In this project I use animations in website with both plain html css or javascript and React JS with usewebanimations. 
+
+
+### Project Description
+
+* is build on another language Javascript
+* to scale project 
+* to avoid run time error
+* to provide type checking
+* code will converts from typescript to javascript
+
+
+### `Website`
+
+Website Link  Open [http://anasali-p4a.surge.sh/](http://anasali-p4a.surge.sh/) to view it in the browser.
+
---
+++ dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+// console.log("new world");
+// let b : number = 334;  // specifying the type of variable
+// b = 96
+// console.log(b)
+// let obj = {
+//     foo : "hello"
+// }
+// console.log(obj.foo)
+// we do implicit typing
+// let arr : number [] = [3,4]
+// console.log(arr[1])
+// functions in typescript
+// function square(b : number){
+//     return b*b
+// }
+// let sq = square(5)
+// console.log(sq)
+// square("s") // error
+// with any ...but not recommended
+// let a : any = 34
+// a = "hello"
+// console.log(a)  
+var obj = {
+    fName: "anas",
+    age: 22
+};
+console.log(obj);
